9868cdf4e168fe96c7a6db274e550ca1376845d2
davidmezzetti <561939+davidmezzetti@users.noreply.github.com>
2023-04-20 12:01:16 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ setup(
     install_requires=[
         "datasets>=2.8.0",
         "tqdm>=4.48.0",
-        "txtai>=5.4.0",
+        "txtai>=5.5.0",
     ],
     classifiers=[
         "License :: OSI Approved :: Apache Software License",
